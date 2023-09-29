8a92d32dcffdf41a1dc2d91a4225ec3f4e18eb7f
Andre Pinto <andrefjpinto@gmail.com>
2023-09-29 15:00:38 +0100

    add variables

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "env" {
+  default = "dev"
+}
+
+variable "name" {
+  default = "iem"
+}
+
+variable "repo" {
+  default = "https://bitbucket.org/cellforce/integration-erp-mes-infra/src"
+  description = "The url of the repository in bitbucket"
+}
